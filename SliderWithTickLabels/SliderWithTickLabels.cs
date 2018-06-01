919912871c8ed388162c38fe2062f5f4a95a43a9
Tiago Costa <tdcosta100@gmail.com>
2018-06-01 18:01:22 -0300

    Adicionar arquivos de projeto.

---
+++ SliderWithTickLabels/SliderWithTickLabels.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SliderWithTickLabels
+{
+    /// <summary>
+    /// Siga os passos 1a ou 1b e depois 2 para usar esse controle personalizado em um arquivo XAML.
+    ///
+    /// Passo 1a) Usando o controle personalizado em um arquivo XAML que já existe no projeto atual.
+    /// Adicione o atributo XmlNamespace ao elemento raiz do arquivo de marcação onde ele 
+    /// deve ser usado:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:SliderWithTickLabels"
+    ///
+    ///
+    /// Passo 1b) Usando o controle personalizado em um arquivo XAML que existe em um projeto diferente.
+    /// Adicione o atributo XmlNamespace ao elemento raiz do arquivo de marcação onde ele 
+    /// deve ser usado:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:SliderWithTickLabels;assembly=SliderWithTickLabels"
+    ///
+    /// Também será necessário adicionar nesse projeto uma referência ao projeto que contém esse arquivo XAML
+    /// e Recompilar para evitar erros de compilação:
+    ///
+    ///     No Gerenciador de Soluções, clique com o botão direito no projeto alvo e
+    ///     "Adicionar Referência"->"Projetos"->[Selecione esse projeto]
+    ///
+    ///
+    /// Passo 2)
+    /// Vá em frente e use seu controle no arquivo XAML.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class SliderWithTickLabels : Slider
+    {
+		public static readonly DependencyProperty GeneratedTicksProperty;
+		public static readonly DependencyProperty TickLabelTemplateProperty;
+
+		[Bindable(true)]
+		public DoubleCollection GeneratedTicks
+		{
+			get
+			{
+				return base.GetValue(SliderWithTickLabels.GeneratedTicksProperty) as DoubleCollection;
+			}
+			set
+			{
+				base.SetValue(SliderWithTickLabels.GeneratedTicksProperty, value);
+			}
+		}
+
+		[Bindable(true)]
+		public DataTemplate TickLabelTemplate
+		{
+			get
+			{
+				return base.GetValue(SliderWithTickLabels.TickLabelTemplateProperty) as DataTemplate;
+			}
+			set
+			{
+				base.SetValue(SliderWithTickLabels.TickLabelTemplateProperty, value);
+			}
+		}
+
+		static SliderWithTickLabels()
+        {
+			SliderWithTickLabels.GeneratedTicksProperty = DependencyProperty.Register("GeneratedTicks", typeof(DoubleCollection), typeof(SliderWithTickLabels), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+			SliderWithTickLabels.TickLabelTemplateProperty = DependencyProperty.Register("TickLabelTemplate", typeof(DataTemplate), typeof(SliderWithTickLabels), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+			FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(SliderWithTickLabels), new FrameworkPropertyMetadata(typeof(SliderWithTickLabels)));
+        }
+
+		protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+		{
+			base.OnPropertyChanged(e);
+
+			var propertyNames = new string[] { "Minimum", "Maximum", "TickFrequency", "Ticks" };
+
+			if (IsInitialized && propertyNames.Contains(e.Property.Name))
+			{
+				CalculateTicks();
+			}
+		}
+
+		protected override void OnInitialized(EventArgs e)
+		{
+			base.OnInitialized(e);
+
+			CalculateTicks();
+		}
+
+		protected override Geometry GetLayoutClip(Size layoutSlotSize)
+		{
+			return ClipToBounds ? base.GetLayoutClip(layoutSlotSize) : null;
+		}
+
+		private void CalculateTicks()
+		{
+			if (this.Ticks != null && this.Ticks.Any())
+			{
+				this.GeneratedTicks = new DoubleCollection(this.Ticks.Union(new double[] { this.Minimum, this.Maximum }));
+			}
+			else
+			{
+				if (this.TickFrequency > 0.0)
+				{
+					this.GeneratedTicks = new DoubleCollection(
+						Enumerable.Range(
+							0,
+							Convert.ToInt32(
+								Math.Ceiling((this.Maximum - this.Minimum) / this.TickFrequency)
+							) + 1
+						)
+						.Select(t => Math.Min(t * this.TickFrequency + this.Minimum, this.Maximum))
+					);
+				}
+			}
+		}
+    }
+}
---
+++ SliderWithTickLabels/TickbarLabelMarginConverter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace SliderWithTickLabels
+{
+	class TickBarLabelMarginConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			try
+			{
+				var slider = values[0] as SliderWithTickLabels;
+				var tickBar = slider.Template.FindName("TopTick", slider) as TickBar;
+
+				var positionMinimum = tickBar.ReservedSpace / 2;
+
+				double scalingValue = 0.0;
+				double left = 0.0;
+				double top = 0.0;
+
+				switch (slider.Orientation)
+				{
+					case Orientation.Horizontal:
+						scalingValue = (tickBar.ActualWidth - tickBar.ReservedSpace) / (tickBar.Maximum - tickBar.Minimum);
+						left = - (tickBar.ActualWidth / 2 - positionMinimum - scalingValue * (System.Convert.ToDouble(values[1]) - tickBar.Minimum)) * 2;
+						top = System.Convert.ToDouble(values[2]);
+						break;
+					case Orientation.Vertical:
+						scalingValue = (tickBar.ActualHeight - tickBar.ReservedSpace) / (tickBar.Maximum - tickBar.Minimum);
+						left = System.Convert.ToDouble(values[1]);
+						top = (tickBar.ActualHeight / 2 - positionMinimum - scalingValue * (System.Convert.ToDouble(values[2]) - tickBar.Minimum)) * 2;
+						break;
+					default:
+						break;
+				}
+
+				if (slider.IsDirectionReversed)
+				{
+					left *= -1.0;
+					top *= -1.0;
+				}
+
+				var thickness = new Thickness
+				{
+					Left = (left <= 0.0) ? left : 0,
+					Top = (top <= 0.0) ? top : 0,
+					Right = (left > 0.0) ? -left : System.Convert.ToDouble(values[3]),
+					Bottom = (top > 0.0) ? -top : System.Convert.ToDouble(values[4])
+				};
+
+				return thickness;
+			}
+			catch (Exception ex)
+			{
+				return null;
+			}
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			return new object[]
+			{
+				null,
+				((Thickness)value).Left,
+				((Thickness)value).Top,
+				((Thickness)value).Right,
+				((Thickness)value).Bottom
+			};
+		}
+	}
+}
